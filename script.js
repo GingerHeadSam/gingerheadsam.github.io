081cfbb8ea7bd9c12c5dbffe14fbf5caf5fd1872
GingerHeadSam <50044700+GingerHeadSam@users.noreply.github.com>
2022-08-28 17:13:11 +0100

    update

--- script.js
+++ script.js
@@ -109,13 +109,13 @@ var APCounter = 0;
 $("#APPortfolio").click(function(){
 	if(APCounter == 0){
 		$("#APPortfolio p").css('display', 'block');
-		$("#APPortfolioOpenButton").css('display', 'block');
+		$("#APOpenButton").css('display', 'block');
 		$("#APPortfolio").css('border-bottom', '0.1em solid darkgray');
 		$("#APPortfolio span").html("&#x25B2;");
 		APCounter = 1;
 	}else {
 		$("#APPortfolio p").css('display', 'none');
-		$("#APPortfolioOpenButton").css('display', 'none');
+		$("#APOpenButton").css('display', 'none');
 		$("#APPortfolio").css('border-bottom', 'none');
 		$("#APPortfolio span").html("&#x25BC;");
 		APCounter = 0;
